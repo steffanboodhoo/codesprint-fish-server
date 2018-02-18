88407f82f89113f506e5ce77f59787f203cbd995
steffanboodhoo <steffan_boodhoo@hotmail.com>
2018-02-18 12:48:20 -0400

    shouierfh

--- fish_app/static/main_app/mainCtrl.js
+++ fish_app/static/main_app/mainCtrl.js
@@ -1,19 +1,22 @@
-angular.module('main',['ngMaterial','ngMessages'])
+angular.module('main',['ngMaterial','ngMessages','ngMap'])
 
 .controller('mainCtrl',['$scope','$rootScope','catchService',
 	function($scope,$rootScope,catchService){
 		window.onload = function(){
-			let map = initMap();
-
+			// let map = initMap();
+			$rootScope.table_data=[];
 			catchService.catch.getCatch(null,
 			function(resp){
-				$rootScope.table_data=resp['data']['data'];
-				console.log($rootScope.table_data)
-				loadMarkers(resp['data'],map)
+				$rootScope.markers_data=resp['data']['data'];
+				
+				
 			},null);
 		}
 
-		
+		$rootScope.getData = function(){
+			$rootScope.table_data = $rootScope.markers_data.slice(3,8);
+			console.log($rootScope.table_data)
+		}
 	}
 ])
 console.log('what the fuck')
--- fish_app/static/map/mapCtrl.js
+++ fish_app/static/map/mapCtrl.js
@@ -1,11 +1,28 @@
 angular.module('main')
 
-.controller('mapCtrl',['$scope','$ ',
-	function($scope, $rootScope,NgMap){
-		NgMap.getMap().then(function(map) {
-		    console.log(map.getCenter());
-		    console.log('markers', map.markers);
-		    console.log('shapes', map.shapes);
+.controller('mapCtrl',['$scope','$rootScope','$compile','NgMap',
+	function($scope, $rootScope, $compile, NgMap){
+		$scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyC__jz_XKUkrgNXlkWvyVUwFMogwj_yzio";
+
+		var vm = this;
+	 	vm.ne, vm.sw;
+	  	NgMap.getMap().then(function(map) {
+	    	console.log('map', map);
+	    	vm.map = map;
 	  	});
+
+	  	vm.click = function(ev){
+	  			console.log(ev)
+	  	}
+	  	$scope.rect_visible = 'None';
+	  	function create_rectangle(c_lat,c_lng){
+          $scope.north= c_lat+0.1
+          $scope.south=c_lat-0.1
+          $scope.east=c_lng+0.1
+          $scope.west=c_lng-0.1
+		}
+		create_rectangle(10.467,-61.355)
+	
+
 	}
 ]);
